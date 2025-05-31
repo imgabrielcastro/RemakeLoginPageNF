3ecf5439667185d37ab3be952c8ad753a38aeaac
Gabriel Castro <josegabrielsouzacastro@gmail.com>
2025-05-31 13:48:27 -0300

    login styles

---
+++ LoginNF/App.js
@@ -0,0 +1,14 @@
+import { StatusBar, SafeAreaView } from "react-native";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes/";
+
+export default function App(){
+    return(
+            <NavigationContainer>
+                <StatusBar barStyle="dark-content" />
+                <Routes />
+            </NavigationContainer>
+    )
+}
