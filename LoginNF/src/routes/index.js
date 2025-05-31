e662d554e85a20251ff3a4229decdf5f5ae872b6
Gabriel Castro <josegabrielsouzacastro@gmail.com>
2025-05-30 23:47:26 -0300

    Structure inserts

---
+++ LoginNF/src/routes/index.js
@@ -0,0 +1,8 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Welcome from '../pages/Welcome'
+import SignIn from '../pages/SignIn'
+
+const Stack = createNativeStackNavigator();
+
+
