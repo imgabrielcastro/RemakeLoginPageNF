f6d55ed0b5c25f2831acbc3e27aaf718142c4618
Gabriel Souza de Castro <110572481+imgabrielcastro@users.noreply.github.com>
2025-06-04 12:22:12 -0300

    create datatimepicker

--- LoginNF/src/pages/SignInPt1/index.jsx
+++ LoginNF/src/pages/SignInPt1/index.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-import { View, StyleSheet, TouchableOpacity } from "react-native"; // Importação do TouchableOpacity
-import { BottomNavigation, Text } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from "react-native"; // Importação do TouchableOpacity
+import { Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native"; // Importação do useNavigation
 import MyTextInput from "../../components/InputTemplate";
 import VStack from "../../components/Stacks/VStack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomStatusBar from "../../components/StatusBar";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const CustomBackButton = () => {
   const navigation = useNavigation();
@@ -17,42 +24,61 @@ const CustomBackButton = () => {
 
   return (
     <TouchableOpacity onPress={handleBackPress} style={styles.backButton}>
-      <Icon name="chevron-left" size={30} color="black"/>
+      <Icon name="chevron-left" size={30} color="black" />
     </TouchableOpacity>
   );
 };
 
+const showDatepicker = () => {
+  setShowModal(true); 
+};
+
 export default function CadastroDados() {
   const [name, setName] = useState("");
-  const [cep, setCep] = useState("");
+  const [dataNascimento, setDataNascimento] = useState("");
   const [phone, setPhone] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
 
   return (
     <>
       <CustomStatusBar backgroundColor="#83239F" barStyle="light-content" />
       <SafeAreaView style={styles.container}>
         <CustomBackButton />
-        <VStack style={{padding: 16, bottom: 30}}>
-            <View style={styles.contain}>
-              <Text variant="headlineMedium" style={styles.title}>
-                Preencha os dados para criar a sua conta.
-              </Text>
-              <MyTextInput
-                label="Nome Completo"
-                value={name}
-                onChangeText={setName}
-                icon="account-circle"
-              />
+        <VStack style={{ padding: 16, bottom: 30 }}>
+          <ScrollView style={styles.contain}>
+            <Text variant="headlineMedium" style={styles.title}>
+              Preencha os dados para criar a sua conta.
+            </Text>
+            <MyTextInput
+              label="Nome completo"
+              value={name}
+              onChangeText={setName}
+              icon="account-circle"
+            />
+            <MyTextInput label="Data de nascimento" value={date} icon="event" />
+            <DateTimePicker
+              value={date}
+              mode="date"
+              display="spinner"
+            />
 
-               <MyTextInput
-                label="Telefone"
-                value={phone}
-                onChangeText={setPhone}
-                icon="phone"
-              />
+            <MyTextInput
+              label="Telefone"
+              value={phone}
+              onChangeText={setPhone}
+              icon="phone"
+              keyboardType="numeric"
+            />
 
-            </View>
-          </VStack>
+            <MyTextInput
+              label="Nome completo"
+              value={name}
+              onChangeText={setName}
+              icon="account-circle"
+            />
+          </ScrollView>
+        </VStack>
       </SafeAreaView>
     </>
   );
@@ -61,7 +87,7 @@ export default function CadastroDados() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
   },
   title: {
     color: "#000",
@@ -70,7 +96,11 @@ const styles = StyleSheet.create({
   backButton: {
     left: 10,
     zIndex: 1,
-    position: 'absolute',
-    top: 10
+    position: "absolute",
+    top: 10,
+  },
+  contain: {
+    width: "100%",
+    height: "100%",
   },
 });
